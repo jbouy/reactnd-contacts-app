95b8a471fcddf9d9bd4964ca0b66d46ff644e18a
James Bouy <jbouy1@gmail.com>
2018-09-23 14:14:23 -0500

    feat(lesson 3): 9 - update state with setState

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -15,8 +15,10 @@ const ListContacts = (props) =>
           <p>{contact.name}</p>
           <p>{contact.handle}</p>
         </div>
-        <button className='contact-remove'>
-          Remove
+        <button 
+          onClick={() => props.onDeleteContact(contact)}
+          className='contact-remove'>
+            Remove
         </button>
       </li>            
     ))}
