8c63d6f9edae058629b138835eb508b60aae95f5
James Bouy <jbouy1@gmail.com>
2018-09-30 21:40:37 -0500

    feat(lesson 5): 4 - The Link Component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 class ListContacts extends Component {
   static propTypes = {
@@ -41,11 +42,10 @@ class ListContacts extends Component {
             value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
-          <a
-            href='#create'
-            onClick={onNavigate}
+          <Link
+            to='/create'            
             className='add-contact'
-          >Add Contact</a>
+          >Add Contact</Link>
         </div>
 
         {showingContacts.length !== contacts.length && (
